9e635b777936535f99fbf167832a3e08755f35ff
sivakumarreddym <info@joindevops.com>
2024-11-25 15:17:48 +0530

    user profile

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -129,3 +129,12 @@ resource "aws_security_group_rule" "bastion_public" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.bastion_sg.id
 }
+
+resource "aws_security_group_rule" "mysql_node" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.node_sg.id
+  security_group_id = module.mysql_sg.id
+}
