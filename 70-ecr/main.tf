10a201b9da3e58ac8ed2074bcbfa4a8b93cc24cf
sivakumarreddym <info@joindevops.com>
2024-11-25 17:04:56 +0530

    user profile

--- 70-ecr/main.tf
+++ 70-ecr/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.project_name}/${var.environment}/backend"
   image_tag_mutability = "MUTABLE"
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -10,7 +10,7 @@ resource "aws_ecr_repository" "backend" {
 resource "aws_ecr_repository" "frontend" {
   name                 = "${var.project_name}/${var.environment}/frontend"
   image_tag_mutability = "MUTABLE"
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
