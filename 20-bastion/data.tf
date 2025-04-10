452118ef02343be59746f6fabbebb86085fca39a
Tejaswi G <65135962+tejaswigoli@users.noreply.github.com>
2025-04-10 17:02:21 +0100

    Update data.tf

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -8,7 +8,7 @@ data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
 
-data "aws_ami" "Amazon Linux 2023 AMI" {
+data "aws_ami" "main" {
 
 	most_recent      = true
 	/*owners = ["973714476881"]*/
