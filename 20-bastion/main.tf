9eb9067d71fb1782666b9b54b41b91b23bbd86ba
Tejaswi G <65135962+tejaswigoli@users.noreply.github.com>
2025-04-11 11:40:35 +0100

    Update main.tf

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,7 +1,7 @@
 module "bastion" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   
-  ami = data.aws_ami.joindevops.id
+  ami = data.aws_ami.main.id
   name = local.resource_name
 
   instance_type          = "t3.micro"
